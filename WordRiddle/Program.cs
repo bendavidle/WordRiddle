a60af63f9a08b41f3020d0ada76bc995f7047dfd
Ben David Le <ben@getacademy.no>
2022-05-06 11:15:47 +0200

    Add project files.

---
+++ WordRiddle/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WordRiddle
+{
+    internal class Program
+    {
+        private static readonly string[] Words;
+
+        static Program()
+        {
+            Words = WordList();
+        }
+
+        static void Main(string[] args)
+        {
+            int wordCount = 200;
+            for (int i = 0; i < wordCount; i++)
+            {
+                string randomWord = GetRandomWord();
+                string matchingWord = FindMatchingWord(randomWord);
+                Console.WriteLine(randomWord + " - " + matchingWord);
+            }
+        }
+
+
+
+        private static string FindMatchingWord(string word)
+        {
+            int substringLength = 3;
+            string wordSubstring = word.Substring(word.Length - substringLength);
+
+            while (substringLength < 5)
+            {
+                foreach (string ord in Words)
+                {
+                    string ordSub = ord.Substring(0, substringLength);
+                    if (ordSub == wordSubstring)
+                    {
+                        return ord;
+                    }
+                }
+                substringLength++;
+            }
+            return "Couldn't find matching word.";
+        }
+
+        private static string GetRandomWord()
+        {
+            Random rand = new Random();
+            return Words[rand.Next(Words.Length)];
+        }
+
+        private static string[] WordList()
+        {
+            string[] ordliste = File.ReadAllLines("ordliste.txt");
+            string previousWord = null;
+            List<string> words = new List<string>();
+            foreach (string ord in ordliste)
+            {
+                string word = ord.Split("\t")[1];
+                if (word != previousWord && word.Length > 7 && !word.Contains("-"))
+                {
+                    words.Add(word);
+                    previousWord = word;
+                }
+            }
+            return words.ToArray();
+        }
+    }
+}
